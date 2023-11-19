9a0a28932fb645a759bc77bf846888388900db8d
Ирина Мартоян <surenira@icloud.com>
2023-11-19 22:22:24 +0400

    StoryBook stories fix

---
+++ src/AppWithRedux.stories.tsx
@@ -0,0 +1,12 @@
+import AppWithRedux from "./AppWithRedux";
+import {Decorator} from "./stories/Decorator";
+
+export default {
+    title: 'AppExample',
+    component: AppWithRedux,
+    decorator: Decorator
+}
+
+export const AppWithReduxExample = () => {
+    return <AppWithRedux/>
+}
